450dcb1878a2ac8a6ea273c8bf66c130a82933ae
Madha <joansjai21@gmail.com>
2023-08-31 12:13:54 +0530

    voting

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main{
+
+    // Function to check if a person is eligible to vote
+    public static boolean isEligibleToVote(int age) {
+        return age >= 18;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
 
-        if (isEven(number)) {
-            System.out.println(number + " is even.");
+        if (isEligibleToVote(age)) {
+            System.out.println("You are eligible to vote!");
         } else {
-            System.out.println(number + " is odd.");
+            System.out.println("You are not eligible to vote.");
         }
 
         scanner.close();
-    }
-
-    public static boolean isEven(int num) {
-        return num % 2 == 0;
     }
 }
